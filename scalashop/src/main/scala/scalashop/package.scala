4ff9c077b8163f1ca2a27b0921a34f73340d4b3a
youling chan <chenyoucyl@gmail.com>
2017-03-07 15:31:02 +0800

    add scalashop in parall coursera

---
+++ scalashop/src/main/scala/scalashop/package.scala
@@ -0,0 +1,57 @@
+
+import common._
+
+package object scalashop {
+
+  /** The value of every pixel is represented as a 32 bit integer. */
+  type RGBA = Int
+
+  /** Returns the red component. */
+  def red(c: RGBA): Int = (0xff000000 & c) >>> 24
+
+  /** Returns the green component. */
+  def green(c: RGBA): Int = (0x00ff0000 & c) >>> 16
+
+  /** Returns the blue component. */
+  def blue(c: RGBA): Int = (0x0000ff00 & c) >>> 8
+
+  /** Returns the alpha component. */
+  def alpha(c: RGBA): Int = (0x000000ff & c) >>> 0
+
+  /** Used to create an RGBA value from separate components. */
+  def rgba(r: Int, g: Int, b: Int, a: Int): RGBA = {
+    (r << 24) | (g << 16) | (b << 8) | (a << 0)
+  }
+
+  /** Restricts the integer into the specified range. */
+  def clamp(v: Int, min: Int, max: Int): Int = {
+    if (v < min) min
+    else if (v > max) max
+    else v
+  }
+
+  /** Image is a two-dimensional matrix of pixel values. */
+  class Img(val width: Int, val height: Int, private val data: Array[RGBA]) {
+    def this(w: Int, h: Int) = this(w, h, new Array(w * h))
+
+    def apply(x: Int, y: Int): RGBA = data(y * width + x)
+
+    def update(x: Int, y: Int, c: RGBA): Unit = data(y * width + x) = c
+  }
+
+  /** Computes the blurred RGBA value of a single pixel of the input image. */
+  def boxBlurKernel(src: Img, x: Int, y: Int, radius: Int): RGBA = {
+    // TODO implement using while loops
+    def average(l : List[Int]) : Int = {
+      l.sum / l.length
+    }
+
+    val bits = List(red _, green _, blue _, alpha _).map(f => average( {
+      for {
+        i <- -radius to radius
+        j <- -radius to radius
+      } yield (clamp(x + i, 0, src.width - 1), clamp(y + j, 0, src.height - 1))
+    }.distinct.map(pair => { f(src.apply(pair._1, pair._2)) }).toList))
+    rgba(bits.head, bits.tail.head, bits.tail.tail.head, bits.tail.tail.tail.head)
+  }
+}
